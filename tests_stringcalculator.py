502d4c3de58c932a637b0194c3652807f17f349c
miguel0 <miguelbarroe@gmail.com>
2021-03-08 20:54:54 -0600

    test

--- tests_stringcalculator.py
+++ tests_stringcalculator.py
@@ -37,8 +37,8 @@ class TestStringMethods(unittest.TestCase):
 			stringcalculator.add("a")
 	
 	def test_negative_args(self):
-		with self.assertRaises(TypeError):
-			stringcalculator.add("-100,20,1000")
+		with self.assertRaises(IndexError):
+			stringcalculator.add("-100,20,-1000,9,-6")
 			stringcalculator.add("-1")
 			stringcalculator.add("a\n-1a34")
 
