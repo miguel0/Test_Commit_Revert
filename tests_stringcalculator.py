66b237fd450ea152d1512203c0be5438a98e6dfa
miguel0 <miguelbarroe@gmail.com>
2021-03-08 20:27:07 -0600

    test

--- tests_stringcalculator.py
+++ tests_stringcalculator.py
@@ -9,6 +9,7 @@ class TestStringMethods(unittest.TestCase):
 	
 	def test_one_args(self):
 		self.assertEqual(stringcalculator.add("3"), 3)
+		self.assertEqual(stringcalculator.add("35"), 35)
 	
 	def test_two_args(self):
 		self.assertEqual(stringcalculator.add("3,7"), 10)
