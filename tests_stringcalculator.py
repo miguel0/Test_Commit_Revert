9206694cd89869a09a80f53c1e671830550b5a53
miguel0 <miguelbarroe@gmail.com>
2021-03-08 20:36:54 -0600

    test

--- tests_stringcalculator.py
+++ tests_stringcalculator.py
@@ -19,7 +19,7 @@ class TestStringMethods(unittest.TestCase):
 		self.assertEqual(stringcalculator.add("3,7,10,20"), 40)
 		self.assertEqual(stringcalculator.add("100,20,1000"), 1120)
 	
-	def test_dif_separators_args(self):
+	def test_two_delimiters_args(self):
 		self.assertEqual(stringcalculator.add("3,7\n10,20"), 40)
 		self.assertEqual(stringcalculator.add("100\n20\n1000"), 1120)
 		with self.assertRaises(ValueError):
