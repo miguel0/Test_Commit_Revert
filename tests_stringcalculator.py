c65435a0acb245aeb1e0f3eabadd74a20fbef73f
miguel0 <miguelbarroe@gmail.com>
2021-03-08 20:28:24 -0600

    test

--- tests_stringcalculator.py
+++ tests_stringcalculator.py
@@ -14,6 +14,10 @@ class TestStringMethods(unittest.TestCase):
 	def test_two_args(self):
 		self.assertEqual(stringcalculator.add("3,7"), 10)
 		self.assertEqual(stringcalculator.add("100,20"), 120)
+	
+	def test_any_args(self):
+		self.assertEqual(stringcalculator.add("3,7,10,20"), 40)
+		self.assertEqual(stringcalculator.add("100,20,1000"), 1120)
 
 
 if __name__ == '__main__':
