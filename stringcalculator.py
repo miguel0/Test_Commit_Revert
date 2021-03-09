33197baa8102f4b5381e30b80577468746e33e1c
miguel0 <miguelbarroe@gmail.com>
2021-03-08 20:49:10 -0600

    progress

--- stringcalculator.py
+++ stringcalculator.py
@@ -2,5 +2,14 @@ def add(s):
 	if not s:
 		return 0
 	
-	s = s.replace('\n', ',')
-	return sum([int(x) for x in s.split(',')])
+	delimiter = ','
+	if not s[0].isdigit():
+		if s[1] == '\n':
+			delimiter = s[0]
+			s = s[2:]
+		else:
+			raise TypeError('bad delimiter formatting')
+	else:
+		s = s.replace('\n', ',')
+
+	return sum([int(x) for x in s.split(delimiter)])
